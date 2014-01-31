d1768ba31108219188e90b15987d08ed35f3b1e2
alexey <alexey@master.hadoop.vm>
2014-01-31 07:39:25 +0100

    31.01.14

--- mapperReverse.py
+++ mapperReverse.py
@@ -2,6 +2,7 @@
 
 import sys
 import re
+import hashlib
 def read_input(files):
 	for line in files:
 	        yield line.strip()
@@ -21,23 +22,26 @@ def main(sep):
 	pattern=re.compile(r"[^a-zA-Z0-9@]+")
 	# read FASTQ file using python iterators
 	lines = read_input(sys.stdin)
-	nLines=0; match = False; headerLocated = False; tmp=0
+	nLines=0; match = False; headerLocated = False; tmp=100
 	for line in lines:
 		if not headerLocated: # looking for the header line
 			if match: headerLocated=True;
 			nLines+=1
 			if (line[0] == '@'):	tmp = nLines
 			elif (line[0] == '+'):
-				if nLines - tmp == 2: match = True; nLines = 0
+				if nLines - tmp == 2: 
+					match = True; nLines = 0
 		else:	
 			if (nLines == 1):	
+				line = ''.join(line.split('.'))
 				if sep: 
-					list.append(line.split(sep)[0]+'.2')
+					list.append('@' + hashlib.sha1(''.join(line.split(sep)[0])).hexdigest()+'.2')
 				else:
-					list.append(''.join(line.split('.')) + '.2')
+					list.append('@'+hashlib.sha1(line).hexdigest() + '.2')
 			#if (nLines == 1):	list.append(''.join(re.split(pattern,line))+'.1')
 			else: list.append(line)
-			if nLines > 3: 
+#			if nLines > 3: 
+                        if (nLines > 3):
 			 nLines =1
 			 print "%s\t%s %s" % (list[0], list[1], list[3])
 			 list = []
