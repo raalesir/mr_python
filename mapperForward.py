c51bb813021b6a15eae7d007a8587f3351f1c5f8
alexey <alexey@master.hadoop.vm>
2014-01-31 07:38:30 +0100

    state to 31 of January

--- mapperForward.py
+++ mapperForward.py
@@ -2,6 +2,8 @@
 
 import sys
 import re
+import  hashlib
+
 def read_input(files):
 	for line in files:
 	        yield line.strip()
@@ -11,38 +13,36 @@ if sep == 'space':
 	sep=' '
 elif sep == 'none':
 	sep=None
-
+mate='.1'
 #print 'the sep is', sep
 
 def main(sep):
 	list =[]
-	# split pattern for FASTQ header
-	#pattern=re.compile("[\s+\\,.:=]")
-	pattern=re.compile(r"[^a-zA-Z0-9@]+")
 	# read FASTQ file using python iterators
 	lines = read_input(sys.stdin)
-	nLines=0; match = False; headerLocated = False; tmp=0
-	for line in lines:
-		if not headerLocated: # looking for the header line
-			if match: headerLocated=True;
-			nLines+=1
-			if (line[0] == '@'):	tmp = nLines
-			elif (line[0] == '+'):
-				if nLines - tmp == 2: match = True; nLines = 0
-		else:	
-			if (nLines == 1):	
-				if sep: 
-					list.append(line.split(sep)[0]+'.1')
-				else:
-					list.append(''.join(line.split('.')) + '.1')
-			#if (nLines == 1):	list.append(''.join(re.split(pattern,line))+'.1')
-			else: list.append(line)
-			if nLines > 3: 
-			 nLines =1
-			 print "%s\t%s %s" % (list[0], list[1], list[3])
-			 list = []
+	nLines=0;  tmp=100
+	while True:
+		line = next(lines)
+		nLines+=1
+		if (line[0] == '@'):	tmp = nLines
+		elif (line[0] == '+'):
+			if nLines - tmp == 2: break 
+	next(lines)
+	while True:
+		try: 
+			line = next(lines)
+			line = ''.join(line.split('.'))
+			if sep: 
+				list.append('@'+ hashlib.sha1(''.join(line.split(sep)[0])).hexdigest()+mate)
 			else:
-			 nLines += 1
+				list.append('@'+hashlib.sha1(line).hexdigest() +mate)
+			list.append(next(lines))
+			_=next(lines)
+			#list.append(next(lines))
+			list.append(next(lines))
+		except StopIteration: break
+		print "%s\t%s %s" % (list[0], list[1], list[2])
+		list = []
 
 if __name__ == "__main__":
 	main(sep)
