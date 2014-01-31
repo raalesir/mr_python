c6bcc98aab131240f05e208e7ad786de36919152
alexey <alexey@master.hadoop.vm>
2014-01-31 07:39:42 +0100

    31.01.14

--- reducerMerge.py
+++ reducerMerge.py
@@ -3,16 +3,24 @@
 import sys
 import itertools
 
+
+def read_input(files):
+        for line in files:
+                yield line.strip()
+
+
 prevId = None
 prevRead = None
-for line in sys.stdin:
+#lines = read_input(sys.stdin)
+for line in sys.stdin: #lines:
 	# the mapper uses tab as the separator between key and value.
 	readID,  read = line.strip().split('\t',1)
 	if prevId:
-		if sum(itertools.imap(str.__ne__, prevId, readID)) <3 :
-#		if prevId.split('.')[0] == readID.split('.')[0]:
+#		if sum(itertools.imap(str.__ne__, prevId, readID)) <3 :
+		if prevId.split('.')[0] == readID.split('.')[0]:
 #  this effectively prints only pair-ended reads omitting ones without
 			print '%s\t%s\t%s' %( prevId, '\t'.join(prevRead.split()), '\t'.join(read.split()) )
+#	print "%s\t%s" %(readID, read)		
 			prevId = None
 		else:
 			prevId = readID
